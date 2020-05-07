bc4cf5c53f9a9f1cb6aed12ad5e1d088dc988640
slimBA1996 <slim.BenAmmar1996@gmail.com>
2020-05-07 09:07:48 -0400

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 
-const routes: Routes = [{ path: '', component: AppComponent },];
+const routes: Routes = [{ path: '', pathMatch: 'full', redirectTo: 'acceuil'},
+                        { path: 'acceuil',   component: AppComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
